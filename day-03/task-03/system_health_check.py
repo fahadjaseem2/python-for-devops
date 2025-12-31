56026ac14334d27045ee35f5d67669c7d1c55799
fahad jaseem <fahadjaseem@fahads-MacBook-Air.local>
2026-01-01 03:02:23 +0530

    adding 2nd time day-03 task

--- day-03/task-03/system_health_check.py
+++ day-03/task-03/system_health_check.py
@@ -1,18 +1,35 @@
-import psutil # Importing the psutil library to monitor system performance
+import psutil
 
-# Function to check CPU usage   
-#def system_thershold():
-    user_usage = int(input("Enter CPU usage percentage threshold (0-100) ;"))
+def get_cpu_threshold():
+    try:
+        threshold = int(input("Enter the CPU threshold (0-100): "))
+        if threshold <0 or threshold >100:
+            raise ValueError("Threshold must be between 0-100")
+        return threshold
+    except ValueError as e:
+        print(f"Invalid Input: {e}" )
+        return None
+        
+def check_cpu_usage(threshold):
+    try:
+        cpu_usage = psutil.cpu_percent(interval=1)
+        print(f"current cpu usage: {cpu_usage}%")
 
-    # getting the current cpu usage
-    cpu_usage = psutil.cpu_percent(interval=1)
-    print ("Current CPU usage: ", cpu_usage)
+        if cpu_usage > threshold:
+            print("⚠️ Warning: CPU usage is above threshold. Admin notification triggered.")
+        else:
+            print("CPU usage is within normal limits.")
+    except Exception as e:
+        print(f"Error while checking CPU usage : {e} ")
 
-    #compare current cpu usage to user defined threshold
-    if cpu_usage > user_usage:
-        print ("Warning: CPU usage is above the threshold and mailed to admin.")
+def main():
+    threshold = get_cpu_threshold()
+    
+    if threshold is not None:
+        check_cpu_usage(threshold)
     else:
-        print ("CPU is operating within normal parameters.")
+        print("Script Exit safely due to invalid input.")
 
-# Calling the function to check system health
-#system_thershold()
+if __name__ == "__main__":
+    main()
+    
